3849a05fa2bcd071f384bca1c08c467acfcfca5f
fumiya <gra.fmatsu8@gmail.com>
2020-11-12 20:02:00 +0900

    add db connection to server

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,18 @@
 import app from "./app";
 import { systemLogger } from "./utils/log";
+import { createConnection } from "typeorm";
 
-const APP_PORT = process.env.APP_PORT || 3000;
+createConnection("default").then((connection) => {
+  const APP_PORT = process.env.APP_PORT || 3000;
 
-const server = app.listen(APP_PORT, () => {
-  systemLogger.info(`Express server has started on port ${APP_PORT}`);
-});
+  const server = app.listen(APP_PORT, () => {
+    systemLogger.info(`Express server has started on port ${APP_PORT}`);
+  });
 
-process.on("SIGTERM", () => {
-  systemLogger.info("SIGTERM signal received: closing HTTP server");
-  server.close(() => {
-    systemLogger.info("HTTP server closed");
+  process.on("SIGTERM", () => {
+    systemLogger.info("SIGTERM signal received: closing HTTP server");
+    server.close(() => {
+      systemLogger.info("HTTP server closed");
+    });
   });
 });
